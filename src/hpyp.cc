6f6f379f2b8f28a06e3f57caa9975d3442a92283
David A Roberts <d@vidr.cc>
2013-04-21 19:40:47 +1000

    various improvements

--- src/hpyp.cc
+++ src/hpyp.cc
@@ -28,14 +28,37 @@ using namespace boost::math;
 
 typedef int word_t;
 
-const int N = 5; // N-gram assumption
-double discount[N]; // d_m
-double beta1[N], beta2[N]; // d_m ~ Beta(beta1[m], beta2[m])
+// undef to fix predictions during testing
+#define TEST_UPDATE
+
+int N = 0; // N-gram assumption
+double discount[10]; // d_m
+double beta1[10], beta2[10]; // d_m ~ Beta(beta1[m], beta2[m])
 const double ALPHA = 0; // concentration param
-double base_dist; // uniform G_0
+
+// uniform base distribution
+int vocab_size = 0;
+#define BASE_DIST(w) (1.0 / vocab_size)
+#define BASE_SAMPLE() (rand() % vocab_size)
+
+int rest_count = 0;
 
 double randu(double a, double b) { // U(a,b)
     return a + (b-a) * (double) rand() / RAND_MAX;
+}
+
+double randbeta(double a, double b) { // Beta(a,b)
+    beta_distribution<> dist(a,b);
+    return quantile(dist, randu(0,1)); // inverse transform
+}
+
+double log2_add(double log_a, double log_b) { // log(a),log(b) -> log(a+b)
+    if(log_a < log_b) return log2_add(log_b, log_a); // ensure log_a >= log_b
+    return log_a + log2(1 + exp2(log_b - log_a)); // log(a(1+b/a))
+}
+
+double log2_sub(double log_a, double log_b) { // log(a-b)
+    return log_a + log2(1 - exp2(log_b - log_a)); // log(a(1-b/a))
 }
 
 struct rest {
@@ -60,7 +83,7 @@ struct rest {
     }
 
     double p_word(word_t w) const {
-        if(parent == NULL) return base_dist;
+        if(parent == NULL) return BASE_DIST(w);
         if(ncust == 0) return parent->p_word(w);
 
         double d = discount[ctxt_len];
@@ -73,6 +96,23 @@ struct rest {
         p += (ALPHA + d * ntab) * parent->p_word(w);
         p /= ALPHA + ncust;
         return p;
+    }
+
+    word_t sample_word(void) const {
+        if(parent == NULL) return BASE_SAMPLE();
+
+        double d = discount[ctxt_len];
+        double U = randu(0, ALPHA + ncust);
+        for(hash_map<word_t,room>::const_iterator i = m.begin();
+                i != m.end(); i++) {
+            word_t w = i->first; const room &m_w = i->second;
+            if(m_w.ncust == 0) continue;
+            U -= m_w.ncust - d * m_w.ntab;
+            if(U <= EPS) return w;
+        }
+
+        // sample from parent with probability propto ALPHA + d * ntab
+        return parent->sample_word();
     }
 
     int sample_tab(word_t w, room &m_w, bool discnt) {
@@ -148,9 +188,9 @@ struct rest {
     }
 
     void resample(void) { // remove and re-add all customers
-        for(hash_map<word_t,room>::iterator i = m.begin(); i != m.end(); i++) {
-            word_t w = i->first;
-            room &m_w = i->second;
+        for(hash_map<word_t,room>::iterator i = m.begin();
+                i != m.end(); i++) {
+            word_t w = i->first; room &m_w = i->second;
             for(int j = 0; j < m_w.ncust; j++) {
                 rm_cust(w, m_w);
                 add_cust(w, m_w);
@@ -163,21 +203,42 @@ struct ctxt_tree {
     rest *r;
     hash_map<word_t,ctxt_tree*> children;
 
-    ctxt_tree() : children() {
+    ctxt_tree(rest *restaurant) : r(restaurant), children() {
 #if !SPARSE_HASH
         children.set_empty_key(-1);
 #endif
     }
 
-    ctxt_tree *get(word_t w) { // extend context to left by w
+    ctxt_tree *insert(word_t w) { // extend context to left by w
         if(!children.count(w)) {
-            children[w] = new ctxt_tree;
-            children[w]->r = new rest(r, r->ctxt_len + 1);
+            rest *r_new = new rest(r, r->ctxt_len + 1);
+            children[w] = new ctxt_tree(r_new);
+            rest_count++;
         }
         return children[w];
     }
 
-    void resample(void) { // reseat all customers
+    rest *insert_context(vector<word_t> &text, int j) {
+        ctxt_tree *ct = this;
+        for(int i = 1; i < N; i++) {
+            if(j-i < 0) break;
+            ct = ct->insert(text[j-i]);
+        }
+        return ct->r;
+    }
+
+    const rest *get_context(vector<word_t> &text, int j) const {
+        const ctxt_tree *ct = this;
+        for(int i = 1; i < N; i++) {
+            if(j-i < 0) break;
+            word_t w = text[j-i];
+            if(!ct->children.count(w)) break; // can't extend context further
+            ct = ct->children.find(w)->second;
+        }
+        return ct->r;
+    }
+
+    void resample_seat(void) { // reseat all customers
         r->resample();
         for(hash_map<word_t,ctxt_tree*>::iterator i = children.begin();
                 i != children.end(); i++)
@@ -205,24 +266,30 @@ struct ctxt_tree {
                 i != children.end(); i++)
             i->second->update_beta();
     }
+
+    void resample_param(void) {
+        for(int i = 0; i < N; i++)
+            beta1[i] = beta2[i] = 1;
+        update_beta();
+        for(int i = 0; i < N; i++)
+            discount[i] = randbeta(beta1[i], beta2[i]);
+    }
+
+    void resample(void) {
+        resample_seat();
+        resample_param();
+    }
 };
 
-void resample_param(ctxt_tree &root) {
-    for(int i = 0; i < N; i++) beta1[i] = beta2[i] = 1;
-    root.update_beta();
-    for(int i = 0; i < N; i++)
-        discount[i] = quantile(beta_distribution<>(beta1[i], beta2[i]), randu(0,1));
-}
-
-int main(void) {
-    ctxt_tree root;
-    root.r = new rest(new rest(NULL, -1), 0);
+int main(int argc, char **argv) {
+    assert(argc == 3);
 
     // read data
-    hash_map<string,int> dict;
+    hash_map<string,word_t> dict;
 #if !SPARSE_HASH
     dict.set_empty_key("");
 #endif
+    vector<string> vocab; vocab.push_back("NULL");
     vector<word_t> text;
     vector<int> word_len;
     for(int i = 0; i < N; i++) { // nulls at beginning of text
@@ -235,53 +302,100 @@ int main(void) {
         if(!dict.count(s)) { // new word
             word_t w = dict.size() + 1;
             dict[s] = w;
+            vocab.push_back(s);
+            assert(vocab[w] == s);
         }
         text.push_back(dict[s]);
         word_len.push_back(strlen(buf));
     }
     printf("%d (out of %d) words in dict\n", (int) dict.size(), (int) text.size());
-    int train_size = (int)(.85 * text.size());
-    int test_size = text.size() - train_size;
+    int test_size = atoi(argv[2]);
+    int train_size = text.size() - test_size;
     printf("%d training, %d testing\n", train_size, test_size);
+    fflush(stdout);
 
     // init params
+    N = atoi(argv[1]);
     for(int i = 0; i < N; i++)
         discount[i] = .5;
-    base_dist = 1.0 / dict.size();
+    vocab_size = dict.size();
+
+    // init restaurant context tree
+    rest *G_0 = new rest(NULL, -1); // base distribution
+    rest *G_eps = new rest(G_0, 0); // context-free dist
+    ctxt_tree root(G_eps);
 
     // add training data
     for(int j = N; j < train_size; j++) {
-        rest *r = root.get(text[j-1])->get(text[j-2])->r;
-        //rest *r = root.get(text[j-1])->get(text[j-2])->get(text[j-3])->r;
+        rest *r = root.insert_context(text, j);
         r->add_cust(text[j]);
     }
+    printf("%d restaurants created\n", rest_count);
 
-    // init test contexts
+    // size of test data
     int test_bytes = 0;
-    for(int j = train_size; j < train_size + test_size; j++) {
-        root.get(text[j-1])->get(text[j-2]);
-        //root.get(text[j-1])->get(text[j-2])->get(text[j-3]);
+    for(int j = train_size; j < train_size + test_size; j++)
         test_bytes += word_len[j] + 1;
-    }
+    printf("%gkB of test data\n", test_bytes/1e3);
+    fflush(stdout);
 
-    int nsamples = 300, burnin = 125;
-    for(int i = 0; i < nsamples; i++) {
+    // Gibbs sampling
+    int iters = 300, burnin = 125;
+    double log_sum_prob = log2(0); // log(sum_H P(w|H))
+    for(int i = 0; i < iters; i++) {
+        printf("ITER %3d: ", i);
         root.resample();
-        resample_param(root);
-        printf("\niter %d:\td0 = %f, d1 = %f, d2 = %f, d3 = %f\n",
-                i, discount[0], discount[1], discount[2], discount[3]);
-        if(i < burnin) continue;
+        for(int j = 0; j < N; j++) printf("d%d = %.3f; ", j, discount[j]);
 
+        // calculate likelihood of test data
         double bits = 0;
         for(int j = train_size; j < train_size + test_size; j++) {
-            rest *r = root.get(text[j-1])->get(text[j-2])->r;
-            //rest *r = root.get(text[j-1])->get(text[j-2])->get(text[j-3])->r;
+#ifdef TEST_UPDATE
+            rest *r = root.insert_context(text, j);
+#else
+            const rest *r = root.get_context(text, j);
+#endif
             double p = r->p_word(text[j]);
             bits += -log2(p);
+#ifdef TEST_UPDATE
+            r->add_cust(text[j]);
+#endif
         }
-        printf("%d / %d = %f bits/char\n", (int) bits, test_bytes, bits / test_bytes);
-        printf("perplexity = 2^%f = %f\n", bits / test_size, exp2(bits / test_size));
+        printf("PPLX 2^%.2f = %.1f\n", bits/test_size, exp2(bits/test_size));
+        fflush(stdout);
+
+#ifdef TEST_UPDATE
+        // remove test data for next iteration
+        for(int j = train_size; j < train_size + test_size; j++) {
+            rest *r = root.insert_context(text, j);
+            r->rm_cust(text[j]);
+        }
+#endif
+
+        if(i >= burnin) // average predictions
+            log_sum_prob = log2_add(log_sum_prob, -bits);
     }
+
+    printf("\nSUMMARY\n");
+    double log_mean = log_sum_prob - log2(iters - burnin); // log(sum P/N)
+    printf("Compression ratio: %f bits/char (%f%%)\n", -log_mean / test_bytes,
+            100.0 * -log_mean / (8*test_bytes));
+    printf("Perplexity: 2^%f = %f\n",
+            -log_mean / test_size, exp2(-log_mean / test_size));
+    fflush(stdout);
+
+    // sample text from model
+    vector<word_t> text_sample;
+    int sample_size = 250;
+    printf("\nSAMPLE TEXT\n... ");
+    for(int j = 0; j < sample_size; j++) {
+        const rest *r = root.get_context(text_sample, j);
+        word_t w = r->sample_word();
+        printf("%s ", vocab[w].c_str());
+        text_sample.push_back(w);
+        fflush(stdout);
+    }
+    printf("...\n");
 
     return 0;
 }
