91ae1cfc54b122c39671b0af01a93a2ac21df95d
David A Roberts <d@vidr.cc>
2014-10-06 15:03:55 +1000

    reduce mem usage

--- include/Trie.h
+++ include/Trie.h
@@ -6,41 +6,35 @@
 
 class Trie {
     R weight, leaf;
-    Trie *child[0x100];
+    std::map <char,Trie> child;
     
     public:
-    Trie() : weight(0), leaf(0) {
-        memset(child, 0, sizeof child);
-    }
+    Trie() : weight(0), leaf(0) {}
     
     void clear() {
         weight = leaf = 0;
-        FOR(c,0x100) if(child[c]) child[c]->clear();
+        FOR_VAL(c,child) c.clear();
     }
     
-    void insert(std::string s, R w) { insert(s.c_str(), w); }
-    void insert(const char *s, R w) {
+    void insert(std::string s, R w) { insert(s.begin(), s.size(), w); }
+    template<typename I>
+    void insert(I s, N len, R w) {
         weight += w;
-        char c = s[0];
-        if(c == '\0') {
-            leaf += w;
-        } else {
-            if(child[c] == NULL) child[c] = new Trie();
-            child[c]->insert(s+1, w);
-        }
+        if(len == 0) leaf += w;
+        else child[*s].insert(s+1, len-1, w);
     }
     
     const Trie *lookup(std::string s) const DO(lookup(s.c_str(), s.size()))
     template<typename I>
     const Trie *lookup(I s, N len) const {
         if(len == 0) return this;
-        if(child[*s] == NULL) return NULL;
-        return child[*s]->lookup(s+1, len-1);
+        IF_FIND(*s,c,child) return c.lookup(s+1, len-1);
+        return NULL;
     }
     
-    R predict(char c = '\0') const {
-        if(c == '\0') return leaf / weight;
-        if(child[c] == NULL) return 0;
-        return child[c]->weight / weight;
+    R predict(char x = 0) const {
+        if(!x) return leaf / weight;
+        IF_FIND(x,c,child) return c.weight / weight;
+        return 0;
     }
 };
